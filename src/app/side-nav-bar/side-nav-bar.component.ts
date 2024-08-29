a653645a28111f2969d7109e401990a40378dc96
bhatnagar444 <bhatnagar444@gmail.com>
2024-08-29 23:50:43 +0530

    Arpit[develop] : Side menu prepared.

--- src/app/side-nav-bar/side-nav-bar.component.ts
+++ src/app/side-nav-bar/side-nav-bar.component.ts
@@ -10,7 +10,6 @@ import { ShowLicenseDetailsComponent } from '../documentation/license/show-licen
 import { ShowDmapNotificationsComponent } from '../documentation/notifications/show-dmap-notifications/show-dmap-notifications.component';
 import { AssessmentLogsComponent } from '../common/Modal/assessment-logs/assessment-logs.component';
 import { DmapMasterEmailSettingsModalComponent } from '../common/Modal/dmap-master-email-settings-modal/dmap-master-email-settings-modal.component';
-import { DmapMasterSettingsModalComponent } from '../common/Modal/dmap-master-settings-modal/dmap-master-settings-modal.component';
 import { DmapAlertDialogModal } from '../common/Modal/dmap-alert-dialog/dmap-alert-dialog.component';
 import { MenuMappingService } from '../common/Services/menu-mapping.service';
 import { CommonServices } from '../common/Services/common-services.service';
@@ -28,6 +27,7 @@ export class SideNavBarComponent implements OnInit {
   sideNavBarOpen: boolean = false;
   visible: boolean = false;
   visibleAssessment: boolean = true;
+  visibleMiscellaneous;
   visibleValidation: boolean = false;
   visibleSettings: boolean = false;
   visibleMisc: boolean = false;
@@ -35,11 +35,10 @@ export class SideNavBarComponent implements OnInit {
   userLogin: string;
   licenseType: any;
   settings_label: any;
-  showArrow1: boolean = true;
-  showArrow2: boolean = false;
+  showArrow1: boolean = false;
+  showArrow2: boolean = true;
   showArrow3: boolean = true;
-  showArrow4: boolean = true;
-  showArrow5: boolean = true;
+
   disableAnalytics: boolean = true;
   appRemediationLive: boolean = false;
   analyticsStatusCalls: any;
@@ -48,8 +47,7 @@ export class SideNavBarComponent implements OnInit {
   appContainerDown: boolean = true;
   appNodeNotConfigured: boolean = true;
   visibleSchemaValidation: boolean = false;
-  showArrow6: boolean = true;
-  showArrow7: boolean = true;
+
   visibleDataMigration: boolean = false;
 
   constructor(
@@ -59,12 +57,16 @@ export class SideNavBarComponent implements OnInit {
     private modalService: NgbModal,
     private databaseListService: DatabaseListService,
     private commonServices: CommonServices,
-    private router: Router,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.commonServices.callMenuControl$.subscribe((data:any) => {
-      if (data != undefined && data === true && sessionStorage.getItem('isLogin')) {
+    this.commonServices.callMenuControl$.subscribe((data: any) => {
+      if (
+        data != undefined &&
+        data === true &&
+        sessionStorage.getItem('isLogin')
+      ) {
         this.getMenuAndPageMapping();
       }
     });
@@ -154,70 +156,54 @@ export class SideNavBarComponent implements OnInit {
     });
   }
 
-  toggleCollapse(): void {
-    this.visible = !this.visible;
-    this.showArrow1 = !this.showArrow1;
-  }
   toggleCollapseAssessment(): void {
     this.visibleAssessment = !this.visibleAssessment;
-    this.showArrow2 = !this.showArrow2;
-  }
-  toggleCollapseValidation(): void {
-    this.visibleValidation = !this.visibleValidation;
-    this.showArrow3 = !this.showArrow3;
+    this.showArrow1 = !this.showArrow1;
   }
   toggleCollapseSettings(): void {
     this.visibleSettings = !this.visibleSettings;
-    this.showArrow4 = !this.showArrow4;
+    this.showArrow2 = !this.showArrow2;
   }
-  toggleCollapseMisc(): void {
-    this.visibleMisc = !this.visibleMisc;
-    this.showArrow5 = !this.showArrow5;
-  }
-  toggleCollapseSchemaValidation():void{
-    this.visibleSchemaValidation = !this.visibleSchemaValidation;
-    this.showArrow6 = !this.showArrow6;
-  }
-  toggleCollapseDataMigration():void{
-    this.visibleDataMigration = !this.visibleDataMigration;
-    this.showArrow7 = !this.showArrow7;
+  toggleCollapseMiscellaneous(): void {
+    this.visibleMiscellaneous = !this.visibleMiscellaneous;
+    this.showArrow3 = !this.showArrow3;
   }
 
   async getDMAPImageDetails() {
     this.spinner.show();
     // this.pingAndGetAppNodeStatus();
 
-    if (this.appContainerDown === false && this.appNodeNotConfigured === false) {
-      let dbRes:any = [];
-      let appRes:any = [];
+    if (
+      this.appContainerDown === false &&
+      this.appNodeNotConfigured === false
+    ) {
+      let dbRes: any = [];
+      let appRes: any = [];
       try {
         dbRes = await this.loginService.getDMAPImageDetails().toPromise();
       } catch (err) {
-        dbRes = [{status: 'failed'}];
+        dbRes = [{ status: 'failed' }];
       }
 
       try {
         // appRes = await this.dmapExtensionScanService.getDMAPAppImageDetails().toPromise();
       } catch (err) {
-        appRes = [{status: 'failed'}];
+        appRes = [{ status: 'failed' }];
       }
-            this.spinner.hide();
-            const modalRef = this.modalService.open(
-              ShowDmapNotificationsComponent,
-              {
-                size: 'lg',
-                scrollable: true,
-              }
-            );
-            modalRef.componentInstance.data = {
-              title: 'DMAP Image Details',
-              imageDetails: dbRes[0],
-              appImageDetails: appRes[0],
-            };
-            modalRef.result.then((result) => {
-              // if (result.status == 'success') {
-              // }
-            });
+      this.spinner.hide();
+      const modalRef = this.modalService.open(ShowDmapNotificationsComponent, {
+        size: 'lg',
+        scrollable: true,
+      });
+      modalRef.componentInstance.data = {
+        title: 'DMAP Image Details',
+        imageDetails: dbRes[0],
+        appImageDetails: appRes[0],
+      };
+      modalRef.result.then((result) => {
+        // if (result.status == 'success') {
+        // }
+      });
     } else {
       this.spinner.show();
       let res = [{ status: 'failed' }];
@@ -322,7 +308,6 @@ export class SideNavBarComponent implements OnInit {
     });
   }
 
-
   masterBackup() {
     const modalRef = this.modalService.open(NgbdConfirmationModal);
     modalRef.componentInstance.data = {
@@ -331,48 +316,34 @@ export class SideNavBarComponent implements OnInit {
       okButtonLabel: 'Continue',
       cancelButtonLabel: 'Cancel',
       label: 'restoreDmap',
-      showRadioButtons: true 
+      showRadioButtons: true,
     };
     modalRef.result.then((result) => {
       if (result == 'ok') {
-        const appBackupRequired = modalRef?.componentInstance?.data?.showRadioButtons 
-        ? modalRef.componentInstance.userChoice === 'yes' 
-        : false;
+        const appBackupRequired = modalRef?.componentInstance?.data
+          ?.showRadioButtons
+          ? modalRef.componentInstance.userChoice === 'yes'
+          : false;
         const progressModalRef = this.modalService.open(
           DmapBackupProgressModalComponent,
           { size: 'lg', scrollable: true, backdrop: 'static' }
         );
-        this.databaseListService.backupDMAP(appBackupRequired).subscribe((res) => {
-          if (res.type == 'application/json' || res.type == "text/html") {
-            progressModalRef.componentInstance.vmBkpStatus = res
-          } else {
-            let blob = new Blob([res], {});
-            let filename = 'dmap_complete.tar.gz';
-            FileSaver.saveAs(blob, filename);
-            this.openAlert(
-              "Backup Downloaded Successfully.",
-              true
-            );
-            this.commonServices.closeMinimizedWindow();           }
-        });
+        this.databaseListService
+          .backupDMAP(appBackupRequired)
+          .subscribe((res) => {
+            if (res.type == 'application/json' || res.type == 'text/html') {
+              progressModalRef.componentInstance.vmBkpStatus = res;
+            } else {
+              let blob = new Blob([res], {});
+              let filename = 'dmap_complete.tar.gz';
+              FileSaver.saveAs(blob, filename);
+              this.openAlert('Backup Downloaded Successfully.', true);
+              this.commonServices.closeMinimizedWindow();
+            }
+          });
       }
     });
   }
-
-
-  // invokeChatGPT(){
-  //   const modalRef = this.modalService.open(ChatGptIntegrationComponent, {
-  //     size: 'lg',
-  //     scrollable: true,
-  //   });
-  //   modalRef.componentInstance.data = {
-  //     title: 'ChatGPT Integration',
-  //   };
-  //   modalRef.result.then((result) => {
-  //     // if (result == 'ok') {
-  //     // }
-  //   });
-  // }
 
   viewLogs() {
     const modalRef = this.modalService.open(AssessmentLogsComponent, {
@@ -382,19 +353,20 @@ export class SideNavBarComponent implements OnInit {
     modalRef.componentInstance.data = {
       title: 'View/Download Worker Node Logs',
     };
-    modalRef.result.then((result) => {
-    });
+    modalRef.result.then((result) => {});
   }
 
   private downloadKit(platform: string, kitType: string): void {
     let downloadObservable: Observable<any>;
-    
+
     if (kitType === 'data_migration') {
       downloadObservable = this.databaseListService.downloadDMKit(platform);
     } else if (kitType === 'schema_validation') {
-      downloadObservable = this.databaseListService.schemaValidationDMKit(platform);
+      downloadObservable =
+        this.databaseListService.schemaValidationDMKit(platform);
     } else if (kitType === 'data_validation') {
-      downloadObservable = this.databaseListService.dataValidationDMKit(platform);
+      downloadObservable =
+        this.databaseListService.dataValidationDMKit(platform);
     }
 
     downloadObservable.subscribe((res) => {
@@ -414,11 +386,11 @@ export class SideNavBarComponent implements OnInit {
   downloadDMKit(platform: string): void {
     this.downloadKit(platform, 'data_migration');
   }
-  
+
   schemaValidationDMKit(platform: string): void {
     this.downloadKit(platform, 'schema_validation');
   }
-  
+
   dataValidationDMKit(platform: string): void {
     this.downloadKit(platform, 'data_validation');
   }
@@ -430,15 +402,7 @@ export class SideNavBarComponent implements OnInit {
     );
     modalRef.componentInstance.data = { title: title, emailType: type };
 
-    // const modalContent = document.querySelector(
-    //   '.modal-content'
-    // ) as HTMLElement;
-    // modalContent.style.width = '527px';
-
-    modalRef.result.then((result) => {
-      // if (result == 'ok') {
-      // }
-    });
+    modalRef.result.then((result) => {});
   }
 
   mailServerSettings(title, type) {
@@ -447,129 +411,15 @@ export class SideNavBarComponent implements OnInit {
       { size: 'lg', scrollable: true }
     );
     modalRef.componentInstance.data = { title: title, emailType: type };
-    modalRef.result.then((result) => {
-      // if (result == 'ok') {
-      // }
-    });
+    modalRef.result.then((result) => {});
   }
 
-  // settings(settings_label) {
-  //   let node_type;
-  //   this.nodeType = sessionStorage.getItem('nodeType');
-  //   if (this.nodeType == 'analytics_master') {
-  //     node_type = 'analytics_master';
-  //   } else {
-  //     node_type = 'dmap_node';
-  //   }
-
-  //   const modalRef = this.modalService.open(DmapMasterSettingsModalComponent, {
-  //     size: 'lg',
-  //     scrollable: true,
-  //   });
-  //   modalRef.componentInstance.data = {
-  //     title: settings_label + ' Settings',
-  //     node_type: node_type,
-  //   };
-  //   modalRef.result.then((result) => {
-  //     // if (result == 'ok') {
-  //     // }
-  //   });
-  // }
-
-  // appSettings() {
-  //   const modalRef = this.modalService.open(AppNodeSettingsComponent, {
-  //     size: 'lg',
-  //     scrollable: true,
-  //   });
-  //   modalRef.result.then((result) => {
-  //     if (result == 'ok') {
-  //       console.log('ok');
-  //     }
-  //   });
-  // }
-
   putActiveClass(subMenuItems, menuName = '') {
-    // if (
-    //   menuName == 'appDashboard' ||
-    //   menuName == 'appAssessment' ||
-    //   menuName == 'appConversion' ||
-    //   menuName == 'appWorkerNodeSetup' ||
-    //   menuName == 'appJreUpgrade' ||
-    //   menuName == 'appMethodMapping'
-    // ) {
-    //   this.loginService.checkAppNodeStatus().subscribe((data) => {
-    //     if (data) {
-    //       console.log('app_node_status_sidenav',data['status']);
-    //       if (data['status'] == 'success') {
-    //         this.appRemediationLive = true;
-    //       } else if (data['status'] == 'app_not_configured') {
-    //         this.appRemediationLive = false;
-    //         this.spinner.hide();
-    //         const modalRef = this.modalService.open(NgbdConfirmationModal);
-    //         modalRef.componentInstance.data = {
-    //           msg: 'Application Remediation node details is not configured.Do you want to setup Application Migration Container ?',
-    //           title: 'Confirmation',
-    //           okButtonLabel: 'Yes',
-    //           cancelButtonLabel: 'No',
-    //         };
-    //         modalRef.result.then((result) => {
-    //           if (result == 'ok') {
-    //             const modalRef = this.modalService.open(
-    //               AddWorkerNodeComponent,
-    //               { size: 'lg', scrollable: true }
-    //             );
-    //             modalRef.componentInstance.data = {
-    //               title: 'Add Master Node',
-    //               type: 'master_app_node',
-    //             };
-    //             modalRef.result.then((result) => {
-    //               if (result == 'ok') {
-    //                 this.spinner.hide();
-    //               }
-    //             });
-    //           }
-    //         });
-    //       } else if (data['status'] == 'network_change'){
-    //         this.appRemediationLive = false;
-    //         this.spinner.hide();
-    //         const modalRef = this.modalService.open(NgbdConfirmationModal);
-    //         modalRef.componentInstance.data = {
-    //           msg: 'Network change detected. Do you want to update the app node details?',
-    //           title: 'Confirmation',
-    //           okButtonLabel: 'Yes',
-    //           cancelButtonLabel: 'No',
-    //         };
-    //         modalRef.result.then((result) => {
-    //           if (result == 'ok') {
-    //             const modalRef = this.modalService.open(
-    //               AddWorkerNodeComponent,
-    //               { size: 'lg', scrollable: true }
-    //             );
-    //             modalRef.componentInstance.data = {
-    //               title: 'Update Master Node',
-    //               type: 'update_app_node',
-    //             };
-    //             modalRef.result.then((result) => {
-    //               if (result == 'ok') {
-    //                 this.spinner.hide();
-    //               }
-    //             });
-    //           } else {
-    //           }
-    //         });
-    //       }
-    //     }
-    //     this.spinner.hide();
-    //   });
-    // } else {
-    //   this.spinner.hide();
-    // }
-
     const buttonEle = document.getElementById(
       subMenuItems
     ) as HTMLButtonElement;
     var btns = buttonEle.getElementsByClassName('d-flex');
-      console.log(btns);
+    console.log(btns);
     for (var i = 0; i < btns.length; i++) {
       btns[i].addEventListener('click', function () {
         var current = document.getElementsByClassName('active');
@@ -631,7 +481,7 @@ export class SideNavBarComponent implements OnInit {
     });
   }
 
-  openAlert(msg, closeAll:boolean) {
+  openAlert(msg, closeAll: boolean) {
     const modalRef = this.modalService.open(DmapAlertDialogModal);
     modalRef.componentInstance.data = { msg: msg, title: 'Alert' };
     modalRef.result.then((result) => {
