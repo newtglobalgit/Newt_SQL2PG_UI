22bc318f9db0ae32d126150352f4433f1c848fde
bhatnagar444 <bhatnagar444@gmail.com>
2024-08-30 18:24:06 +0530

    Arpit[develop] : Test source connection API made with service file

---
+++ src/app/common/Services/sql2pg.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AppConfigService } from 'src/app/common/Services/app-config.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Sql2PgService {
+  constructor(private http: HttpClient, private config: AppConfigService) {}
+
+  private setHeaders(): HttpHeaders {
+    return new HttpHeaders().set('Content-Type', 'application/json');
+  }
+
+  testSourceDbConnection(sourceDbdetails: any): Observable<any> {
+    return this.postWithHeadersSourceDbdetails(
+      this.config.host + '/testSourceDB',
+      sourceDbdetails
+    );
+  }
+
+  private postWithHeadersSourceDbdetails(
+    url: string,
+    sourceDbdetails: any
+  ): Observable<any> {
+    return this.http
+      .post(url, sourceDbdetails, {
+        headers: this.setHeaders(),
+      })
+      .pipe((data) => data);
+  }
+}
