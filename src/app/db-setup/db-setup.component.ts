026d02168295203483c35c3650436cffe6ae5aee
bhatnagar444 <bhatnagar444@gmail.com>
2024-08-30 13:35:23 +0530

    Arpit[develop] : DBSetup page UI made

--- src/app/db-setup/db-setup.component.ts
+++ src/app/db-setup/db-setup.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-db-setup',
   templateUrl: './db-setup.component.html',
-  styleUrls: ['./db-setup.component.css']
+  styleUrls: ['./db-setup.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DbSetupComponent implements OnInit {
+  sourceDBTypeValue: any = 'MsSql';
+  targetDBTypeValue: any = 'Postgres';
+  sourceDbs: any = ['MsSql'];
+  targetDbs: any = ['Postgres'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
