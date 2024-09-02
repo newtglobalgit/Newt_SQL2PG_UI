c32ff4d393e4a985ef224eb511d5c476973751d7
hemag110 <hemag@newtglobalcorp.com>
2024-09-02 22:37:32 +0530

    update on gear-icon

--- src/app/common/Modal/update-password/update-password.component.ts
+++ src/app/common/Modal/update-password/update-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -11,140 +11,92 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class UpdatePasswordComponent implements OnInit {
 
 
-  formpassword: string | null = null;
+
+  selectedValue: string | null = null;  
+  updatePasswordForm: FormGroup;
+  passwordSelected = false;
+  selectedType = "";
   sourceForm: FormGroup;
   targetForm: FormGroup;
 
-  constructor(private fb: FormBuilder,
-    private activeModal: NgbActiveModal,
+  constructor(private fb: FormBuilder
+    , private cd: ChangeDetectorRef,
+    public activeModal: NgbActiveModal,
+    private modalService: NgbModal,
+    private spinner: NgxSpinnerService,
+   
   ) {
     this.sourceForm = this.fb.group({
       sourcePasswordValue: ['', Validators.required],
-      sourceConfirmPasswordValue: ['', Validators.required]
+      sourceConfirmPasswordValue: ['', Validators.required],
+    }, {
+      validator: this.mustMatch('sourcePasswordValue', 'sourceConfirmPasswordValue')
     });
 
     this.targetForm = this.fb.group({
       targetPasswordValue: ['', Validators.required],
-      targetConfirmPasswordValue: ['', Validators.required]
+      targetConfirmPasswordValue: ['', Validators.required],
+    }, {
+      validator: this.mustMatch('targetPasswordValue', 'targetConfirmPasswordValue')
     });
   }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
-  selectDB(db: string) {
-    this.formpassword = db;
+  onRadioChange(value: string) {
+    this.selectedValue = value;
+    this.cd.detectChanges(); 
   }
 
-  clearSelection() {
-    this.formpassword = null;
-    this.sourceForm.reset();
-    this.targetForm.reset();
+
+  submit() {
+    if (this.selectedValue === 'source') {
+      const sourcePassword = this.sourceForm.value.sourcePasswordValue;
+      const sourceConfirmPassword = this.sourceForm.value.sourceConfirmPasswordValue;
+
+      this.passToBackend({ password: sourcePassword, confirmPassword: sourceConfirmPassword });
+    } else if (this.selectedValue === 'target') {
+      const targetPassword = this.targetForm.value.targetPasswordValue;
+      const targetConfirmPassword = this.targetForm.value.targetConfirmPasswordValue;
+
+      this.passToBackend({ password: targetPassword, confirmPassword: targetConfirmPassword });
+    }
   }
 
-// sourcecolmd6: boolean = false;
-// sourcecolmd12: boolean = true;
-// data: any;
-// f: any;
-// selectedOption: string | null = null;
-// formpassword: any = null; // Track the selected radio button value
-// selectedDB: string = ''; // Track which DB's password fields are shown (source or target)
-
-//   // Forms for source and target password validation
-// sourceForm: FormGroup;
-// targetForm: FormGroup;
-
-
-
-//   constructor(private activeModal: NgbActiveModal,
-//     private formBuilder: FormBuilder,
-//     // private databaseListService: DatabaseListService,
-//     private spinner: NgxSpinnerService,
-//     private modalService: NgbModal,
-//     ) { }
-
-
-//   ngOnInit(): void {
-//     // Initialize forms with empty values and validations
-//     this.sourceForm = this.formBuilder.group({
-//       sourcePasswordValue: ['', Validators.required],
-//       sourceConfirmPasswordValue: ['', [Validators.required, this.mustMatch('sourcePasswordValue')]]
-//     });
-
-//     this.targetForm = this.formBuilder.group({
-//       targetPasswordValue: ['', Validators.required],
-//       targetConfirmPasswordValue: ['', [Validators.required, this.mustMatch('targetPasswordValue')]]
-//     });
-//   }
-
-
-//    // Handle radio button selection and update the selectedDB variable
-//    selectDB(dbType: string) {
-//     this.selectedDB = dbType; // Show appropriate password fields
-//     console.log(this.selectedDB)
-//     // Reset the forms when switching between source and target
-//     this.sourceForm.reset();
-//     this.targetForm.reset();
-//   }
-
-//   // Custom validator to match password and confirm password fields
-//   mustMatch(passwordControlName: string) {
-//     return (control: any) => {
-//       const formGroup = control.parent;
-//       if (formGroup) {
-//         const passwordControl = formGroup.get(passwordControlName);
-//         if (passwordControl && passwordControl.value !== control.value) {
-//           return { mustMatch: true };
-//         }
-//       }
-//       return null;
-//     };
-//   }
-
-//   // Dummy method to test source DB connection
-//   testSourceDbConnection() {
-//     if (this.sourceForm.valid) {
-//       console.log('Source DB connection tested.');
-//     }
-//   }
-
-
-
-//   // Dummy method to test target DB connection
-//   testTargetDbConnection() {
-//     if (this.targetForm.valid) {
-//       console.log('Target DB connection tested.');
-//     }
-//   }
-
-//   // Dummy submit method for source DB password
-//   submitSource() {
-//     if (this.sourceForm.valid) {
-//       console.log('Source password submitted:', this.sourceForm.value);
-//     }
-//   }
-
-//   // Dummy submit method for target DB password
-//   submitTarget() {
-//     if (this.targetForm.valid) {
-//       console.log('Target password submitted:', this.targetForm.value);
-//     }
-//   }
-
- 
-
-//   clearTarget() {
-//     this.formpassword = null;
-//     this.selectedOption = null;
-//   }
-  
-  
-  ok(){
-    this.activeModal.close();
+  passToBackend(data: any) {
+    console.log('Passing data to backend:', data);
   }
 
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  clearForm() {
+    if (this.selectedValue === 'source') {
+      this.sourceForm.reset();
+    } else if (this.selectedValue === 'target') {
+      this.targetForm.reset();
+    }
+  }
   cancel() {
     this.activeModal.close('cancel');
   }
-}
+ 
 
+
+  
+}
